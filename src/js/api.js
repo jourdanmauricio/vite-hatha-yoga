6b884105ba601c51091d3ce475e9ed5bbec14848
Mauricio <jourdanmauricio@gmail.com>
2022-11-30 07:59:44 -0300

    Add fetch lessons

---
+++ src/js/api.js
@@ -0,0 +1,21 @@
+export default async function api() {
+  const lessonsId = document.getElementById("lessons-table");
+  const API_URL = `${import.meta.env.VITE_BACK_API}/lessons`;
+
+  const res = await fetch(API_URL);
+  const lessons = await res.json();
+
+  const lessonsTbody = document.createElement("tbody");
+  lessons.forEach((lesson) => {
+    const lessonsTr = document.createElement("tr");
+    const lessonsTd = document.createElement("td");
+    lessonsTd.innerText = lesson.days;
+    const lessonsTd2 = document.createElement("td");
+    lessonsTd2.innerText = lesson.hours;
+    lessonsTr.appendChild(lessonsTd);
+    lessonsTr.appendChild(lessonsTd2);
+    lessonsTbody.appendChild(lessonsTr);
+  });
+
+  lessonsId.appendChild(lessonsTbody);
+}
--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,12 @@
 const d = document;
 import menu from "./menu";
 import form from "./form";
+import api from "./api";
 
 /* Cuando finaliza load */
 d.addEventListener("DOMContentLoaded", (e) => {
   menu();
   form("#form-subscribe");
   form("#form-contact");
+  api();
 });
